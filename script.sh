210c33c0b73441566be828191a4152f456eec46f
raphaelmondo <raphaelmondo15@email.com>
2025-11-21 11:22:50 +0100

    adding loops

--- script.sh
+++ script.sh
@@ -6,15 +6,28 @@ min=1
 
 #Generate a random variable between  1 to 10 
 number=$(( RANDOM % (max - min + 1) + min ))
+ #initializs the number of tries
+ tries=5
+  
+echo "You are welcome to the guessing game"
+echo "guess a value from 1 to 10"
+
+while (true); do
 
 read -r -p "Enter the number " x
 
 if [[ $x -eq $number ]]; then
    echo "you win"
+   break
 elif [[ $x -lt $min ]]; then
     echo "you lost"
+    continue
 elif [[ $x -gt $max ]]; then
     echo "you lost"
+    continue
 else
-    echo "you lost, the random number was $number"
-fi               
+    echo "you lost, try again"
+fi
+done
+echo "the win is $number"
+          
